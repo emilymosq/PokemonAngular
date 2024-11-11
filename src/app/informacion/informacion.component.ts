297e88648534455acdd913f77d34b5fb479b4700
emily <emilymosq@gmail.com>
2024-11-11 09:30:48 +0100

    infomacion de pokemons

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../services/interfaces/pokemon'
 
 @Component({
   selector: 'app-informacion',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './informacion.component.scss'
 })
 export class InformacionComponent {
-
+  pokemons: Pokemon[] = [
+    { id: 1, nombre: 'Bulbasaur', descripcion: 'Es un Pokémon de tipo planta y venenoso.', image_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png' },
+    { id: 2, nombre: 'Charmander', descripcion: 'Es un Pokémon de tipo fuego.', image_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png' },
+    { id: 3, nombre: 'Squirtle', descripcion: 'Es un Pokémon de tipo agua.', image_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png' },
+    { id: 4, nombre: 'Pikachu', descripcion: 'Es un Pokémon de tipo electrico.', image_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png' },
+  ]
 }
